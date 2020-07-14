16fda02885cc15facdf93059a67d54160bacb185
Matheus Izvekov <mizvekov@gmail.com>
2020-07-14 16:41:09 -0300

    change to offset / size in bytes instead of bits
    Also extracts the integer values using char reads

--- luaunpack.c
+++ luaunpack.c
@@ -20,18 +20,17 @@ static packet_t *verify_mem_access(lua_State *L, unsigned int *offset, unsigned 
 
 	*offset = lua_tointeger(L, 2);
 	*size = lua_tointeger(L, 3);
-	if(packet->offset + *offset + *size / 8 >= packet->length)
+	if(packet->offset + *offset + *size > packet->length)
 		return NULL;
 
 	return packet;
 }
 
 static int unpackint(lua_State *L) {
-	unsigned int value;
 	unsigned int offset;
 	unsigned int size;
 	packet_t *packet;
-	int *ptr;
+	unsigned char *ptr;
 
 	packet = verify_mem_access(L, &offset, &size);
 	if (packet == NULL)
@@ -39,21 +38,9 @@ static int unpackint(lua_State *L) {
 
 	ptr = packet->data + offset;
 	switch (size) {
-		case 4:
-			value = *ptr;
-			value &= 0x0F;
-			lua_pushinteger(L, value);
-			break;
-		case 8:
-			value = *ptr;
-			value &= 0xFF;
-			lua_pushinteger(L, value);
-			break;
-		case 16:
-			value = *((unsigned short *) ptr);
-			value &= 0xFFFF;
-			lua_pushinteger(L, htons(value));
-			break;
+		case 1: lua_pushinteger(L, ptr[0]);        break;
+		case 2: lua_pushinteger(L, (ptr[0] << 8) | ptr[1]); break;
+		default: luaL_error(L, "invalid size");
 	}
 
 	return 1;
@@ -68,7 +55,7 @@ static int unpackstring(lua_State *L) {
 	if (packet == NULL)
 		return luaL_error(L, "trying to access out of bounds memory");
 
-	lua_pushlstring(L, packet->data + offset, size / 8);
+	lua_pushlstring(L, packet->data + offset, size);
 
 	return 1;
 }
